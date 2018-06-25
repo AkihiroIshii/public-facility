5ee0b0d1680586849ffdd329b12a07548e3ca2be
= <ishii.akihiro@gmail.com>
2018-06-25 22:32:52 +0900

    add validation presence

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,7 @@
 class Building < ApplicationRecord
   belongs_to :last_updated_user, :class_name => "User"
-  
+
   has_many :relationships, :dependent => :destroy
-  
-  validates :building_number, uniqueness: true
+
+  validates :building_number, presence: true, uniqueness: true
 end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,7 +1,7 @@
 class Facility < ApplicationRecord
   belongs_to :last_updated_user, :class_name => "User"
-  
+
   has_many :relationships, :dependent => :destroy
-  
-  validates :facility_number, uniqueness: true
+
+  validates :facility_number, presence: true, uniqueness: true
 end
